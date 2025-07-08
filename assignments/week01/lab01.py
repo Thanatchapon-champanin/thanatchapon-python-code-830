48980acb294ad71f28bb901174e370660489cc00
Thanatchapon Champanin <thanatchapon.ch@ku.th>
2025-07-08 12:00:20 +0700

    edit profile creator

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,21 @@
+# Exercise 1: Personal Profile Creator
+print("=== Personal Profile Creator ===")
+# Create a program that asks for:
+# - Full name
+# - Age
+# - Email
+# - Phone number
+# - Favorite hobby 
+# Then display it as a profile
+
+name = ("What's your name: ")
+age = ("How old are you: ")
+email = ("What's your email: ")
+phone_number = ("What's your Phone number: ")
+favorite_hobby = ("What's your Favorite hobby: ")
+
+print("==Your profile==")
+print("Name: ", name)
+print("Email: ", age)
+print("Phone number: ", phone_number)
+print("Favorite hobby: ", favorite_hobby)
