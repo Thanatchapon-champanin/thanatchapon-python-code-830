8c84216b60ec3aad0e989ed96f1770356eb6c437
Thanatchapon Champanin <thanatchapon.ch@ku.th>
2025-07-22 11:54:34 +0700

    2 lab

---
+++ assignments/week03/lab02.py
@@ -0,0 +1,26 @@
+balance = 1000
+pin = "1234"
+entered_pin = input("Enter PIN: ")
+if entered_pin == pin:
+    print("PIN accepted")
+    while True:
+        print("\n1. Check Balance")
+        print("2. Withdraw")
+        print("3. Deposit") 
+        print("4. Exit")
+        choice = input("Choose option: ")
+        if choice == "1":
+            print("Your balance:",balance)
+        elif choice == "2":
+            withdraw = float(input("Enter the amount you want:"))
+            balance= balance - withdraw
+            print("remaining amount:",balance)
+        elif choice == "3":
+            deposit = float(input("Enter tyour desired deposit:"))
+            balance= balance+deposit
+            print("remaining amount:",balance)
+        else:
+            print("Exit")
+            break
+else:
+    print("Invalid PIN")
